b7c83dca1cb11213255d1768f622b738a2a22909
Jeff <54901994+GreyLimit@users.noreply.github.com>
2022-10-28 15:45:32 +0100

    Added license

--- replay_i2c.c
+++ replay_i2c.c
@@ -1,4 +1,23 @@
 /*
+Replay_I2C: post-processes data from the LogiclSampler to include I2c data.
+
+Copyright (C) 2022  Jeff Penfold
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
  *	Replay an arduino logic analyser output
  *	converting specific pin data into the
  *	I2C commands/actions that the analyser
